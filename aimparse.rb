f0a296020a3b854b9333d9ebbe1b8086b94accb3
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-05-26 17:54:24 -0700

    repeat names instead of padding

--- aimparse.rb
+++ aimparse.rb
@@ -7,31 +7,28 @@ class AimParse
     self.input = `pbpaste`
     self.names = names
     self.output = []
-    determine_padding
   end
 
   def parse
     names.each do |name|
-      input.gsub!("#{name}\n", "#{name}: ")
+      input.gsub!(/^#{name}\n/, "#{name}: ")
       input.gsub!(/^\d{1,2}:\d{2}\n/, "")
     end
-    pad_output
+
+    current_name = ""
+
+    input.each_line do |line|
+      matches = line.match(/^([\w\s]+): /)
+      if matches
+        current_name = matches[1]
+      else
+        line = "#{current_name}: #{line}"
+      end
+      output.push(line)
+    end
   end
 
   def to_s
     output.join
-  end
-
-  private
-
-  def determine_padding
-    self.padding = " " * (self.names.max.length + 2)
-  end
-
-  def pad_output
-    input.each_line do |line|
-      line = padding + line unless line.start_with?(*names)
-      output.push(line)
-    end
   end
 end
